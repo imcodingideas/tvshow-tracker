bb8165b6fbd951d71bf0141cc2fdc938af51df67
Joseph Chambers <joseph@michael-chambers.com>
2018-05-11 15:36:09 -0700

    Fixed card, filtering show data

--- src/client/components/home/Home.js
+++ src/client/components/home/Home.js
@@ -2,27 +2,91 @@ import React, { Component, Fragment } from 'react'
 import Navbar from '../main/Navbar/Navbar'
 import '../../scss/styles.scss'
 
+const uuidv4 = require('uuid/v4')
+
 export default class Home extends Component {
   constructor(props) {
     super(props)
-    this.state = { username: null }
+    this.state = {
+      headingTitle: null,
+      genres: [],
+      alphabet: [],
+    }
+    this.filterByGenre = this.filterByGenre.bind(this)
   }
 
   componentDidMount() {
-    fetch('/api/getUsername')
+    fetch('/api/shows')
       .then(res => res.json())
-      .then(user => this.setState({ username: user.username }))
+      .then(data => {
+        const { headingTitle, genres, alphabet } = data
+        this.setState({
+          headingTitle,
+          genres,
+          alphabet,
+        })
+      })
+  }
+
+  filterByGenre(searchTerm) {
+    this.setState(prevState => ({
+      genres: prevState.genres.filter(genre => genre !== searchTerm),
+      headingTitle: searchTerm,
+    }))
   }
 
   render() {
     return (
       <Fragment>
         <Navbar />
-        {this.state.username ? (
-          <h1>Hello {this.state.username}</h1>
-        ) : (
-          <h1>Loading.. please wait!</h1>
-        )}
+        <div className="jumbotron">
+          <div className="container">
+            <ul className="alphabet">
+              {this.state.alphabet.map(char => (
+                <li key={uuidv4()}>
+                  <span>{char}</span>
+                </li>
+              ))}
+            </ul>
+            <ul className="genres">
+              {this.state.genres.map(genre => (
+                <li key={uuidv4()}>
+                  <span>{genre}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        <div className="container">
+          <div className="card">
+            <div className="card-header">
+              {this.state.headingTitle}
+              <div className="float-right">
+                <input
+                  name="queryName"
+                  className="search"
+                  onChange={key => this.filterByGenre(key.target.value)}
+                  type="text"
+                  placeholder="Search..."
+                />
+              </div>
+            </div>
+            <div className="card-body">
+              <div className="row show-list">
+                <div className="col-xs-4 col-md-3">
+                  <a href="/shows/{{show._id}}">
+                    <img className="img-rounded" width="100%" />
+                  </a>
+                  <div className="text-center">
+                    <a href="/shows/{{show._id}}">ShowName</a>
+                    <p className="text-muted">Episodes: show.episodes.length</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </Fragment>
     )
   }
--- src/server/index.js
+++ src/server/index.js
@@ -4,7 +4,67 @@ const os = require('os')
 const app = express()
 
 app.use(express.static('dist'))
-app.get('/api/getUsername', (req, res) =>
-  res.send({ username: os.userInfo().username })
-)
+
+app.get('/api/shows', (req, res) => {
+  const alphabet = [
+    '0-9',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ]
+  const genres = [
+    'Action',
+    'Adventure',
+    'Animation',
+    'Children',
+    'Comedy',
+    'Crime',
+    'Documentary',
+    'Drama',
+    'Family',
+    'Fantasy',
+    'Food',
+    'Home and Garden',
+    'Horror',
+    'Mini-Series',
+    'Mystery',
+    'News',
+    'Reality',
+    'Romance',
+    'Sci-Fi',
+    'Sport',
+    'Suspense',
+    'Talk Show',
+    'Thriller',
+    'Travel',
+  ]
+
+  const headingTitle = 'Top 12 Shows'
+
+  res.send({ headingTitle, genres, alphabet })
+})
+
 app.listen(8080, () => console.log('Listening on port 8080!'))
